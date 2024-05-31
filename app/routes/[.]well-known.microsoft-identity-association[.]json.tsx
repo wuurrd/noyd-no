396a9b84f604abd7f751d539125c4a1e0fe7564f
SMU <gh@smuco.de>
2024-05-31 15:05:05 +0200

    new frontpage

--- app/routes/[.]well-known.microsoft-identity-association[.]json.tsx
+++ app/routes/[.]well-known.microsoft-identity-association[.]json.tsx
@@ -1,9 +1,9 @@
-export function loader(req: Request) {
-    return {
-        associatedApplications: [
-            {
-                applicationId: "d55db4fe-a54a-4b82-9723-e8559afeeabd",
-            },
-        ],
-    };
+export function loader() {
+  return {
+    associatedApplications: [
+      {
+        applicationId: "d55db4fe-a54a-4b82-9723-e8559afeeabd",
+      },
+    ],
+  };
 }
