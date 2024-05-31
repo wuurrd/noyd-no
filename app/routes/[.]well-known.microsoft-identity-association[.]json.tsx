c888eb702759bd244a74863dac93600388677fd9
David Buchmann <david.buchmann@gmail.com>
2024-05-31 11:59:34 +0200

    add microsoft verification

---
+++ app/routes/[.]well-known.microsoft-identity-association[.]json.tsx
@@ -0,0 +1,9 @@
+export function loader(req: Request) {
+    return {
+        associatedApplications: [
+            {
+                applicationId: "eed879d7-8243-4c3d-9085-596740453a42",
+            },
+        ],
+    };
+}
