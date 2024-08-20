ff48808520dc01686f139295f8a597814cba51dd
David Buchmann <david.buchmann@gmail.com>
2024-08-20 16:47:40 +0200

    pages: Try without a folder at base

--- astro.config.mjs
+++ astro.config.mjs
@@ -14,7 +14,7 @@ import vercel from "@astrojs/vercel/serverless";
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.noyd.no",
-  output: "server",
+  output: "hybrid",
   adapter: vercel(),
   redirects: {
     "/admin": "/keystatic",
